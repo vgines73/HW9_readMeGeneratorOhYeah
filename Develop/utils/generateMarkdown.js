1a1e9dbea4f4d0d13c189399b6311ff7ea827082
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-11-25 12:59:55 -0800

    added readme markdown initial setup. still need to figure out how to create links to table of contents.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description:
+  ${data.description}
 
+## Table of Contents:
+
+  *Installation 
+  *Usage 
+  *License 
+  *Contributing 
+  *Tests
+  *Questions
+
+## Installation:
+  ${data.installationInstructions}
+
+## Usage:
+  ${data.usageInfo}
+
+## License:
+  ${data.license}
+
+## Contributing:
+  ${data.contribution}
+
+## Test Instructions:
+  ${data.testInstructions}
+
+## Questions:
+  Link to my GitHub profile <a href="https://github.com/${data.gitHub}">${data.gitHub}</a>
+  You can reach me by email at: ${data.email}
 `;
 }
 
